564b7bb35f179fb777142a8cf96ea9e86a288567
Varisht524 <varishtdixit@gmail.com>
2025-06-05 16:30:47 -0500

    Commit DoNow.py

--- Lesson3/DoNow.py
+++ Lesson3/DoNow.py
@@ -1,3 +1,6 @@
-favorite_season =  'spring'
-favorite_temperature = '92'  #degrees
-print(f' My favorite season is ' + {favorite_season} + ' and I love when it is' + {favorite_temperature} + ' degrees outside ')
+
+favorite_season = 'spring'
+favorite_temperature = '92'  # degrees
+print(f'My favorite season is {favorite_season} and I love when it is {favorite_temperature}  degrees outside')
+
+
